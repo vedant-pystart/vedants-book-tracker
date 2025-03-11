3dae7ad4d43714bd3adac374f119588b86201bbf
Vedant <connectvedantj@gmail.com>
2025-03-11 00:53:01 -0500

    Addedhosting for dash

--- Sheetsbooktracker.py
+++ Sheetsbooktracker.py
@@ -417,6 +417,5 @@ def update_vis(rec_values, year_values, month_values):
     return vis
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run_server(debug=True, host='0.0.0.0', port=5000)
 
