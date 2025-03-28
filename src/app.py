c0ec55b59505d7d58c714aece1cc986c2c7bf6a4
Vedant <connectvedantj@gmail.com>
2025-03-27 22:36:57 -0500

    Commented local df code

--- src/app.py
+++ src/app.py
@@ -56,7 +56,7 @@ import pandas as pd
 import pandas_datareader.data as web
 import datetime
 
-df = pd.read_excel('Past_Projects/GIT Local Book Tracker/Book Log.xlsx')
+# df = pd.read_excel('Past_Projects/GIT Local Book Tracker/Book Log.xlsx')
 
 """
 ==================================================
